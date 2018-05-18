66529c9858f85861edd1ee95e3f30f53cd912349
GittyGatto <rhenning80@gmx.de>
2018-05-18 23:19:32 +0200

    Refactoring

--- webapp/src/main/java/com/henning/donkey/business/ArticleBusinessService.java
+++ webapp/src/main/java/com/henning/donkey/business/ArticleBusinessService.java
@@ -1,78 +1,27 @@
 package com.henning.donkey.business;
 
 import com.henning.donkey.business.responseEnties.ArticleDto;
-import com.henning.donkey.business.responseEnties.CartArticleDto;
+import com.henning.donkey.business.transformer.ArticleTransformerService;
 import com.henning.donkey.domain.article.ArticleEntity;
 import com.henning.donkey.domain.article.ArticleRepository;
-import com.henning.donkey.domain.cart.CartEntity;
-import com.henning.donkey.domain.cart.CartRepository;
-import com.henning.donkey.domain.cartArticle.CartArticleEntity;
-import com.henning.donkey.domain.cartArticle.CartArticleRepository;
-import com.henning.donkey.domain.category.CategoryEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ArticleBusinessService {
 
     @Autowired
-    private CartArticleRepository cartArticleRepository;
-
-    @Autowired
-    private CartRepository cartRepository;
-
+    private ArticleTransformerService articleTransformerService;
     @Autowired
     private ArticleRepository articleRepository;
 
     @Autowired
     private CategoryBusinessService categoryBusinessService;
 
-    /*
-    public List<CartArticleDto> getCartArticles(String cartName) {
-        CartEntity cartEntity = this.cartRepository.findByCartName(cartName);
-        List<CartArticleEntity> cartArticleEntities = this.cartArticleRepository.findByCart(cartEntity);
-        return toCartArticles(cartArticleEntities);
-    }
-    */
-
     public List<ArticleDto> getArticles() {
         List<ArticleEntity> articleEntities = this.articleRepository.findAll();
-        return toArticles(articleEntities);
-    }
-
-    public List<ArticleDto> getCategoryArticles(String categoryName) {
-        if (categoryName.equals("All")) {
-            return getAllArticles();
-        } else {
-            return getFilteredCategoryArticles(categoryName);
-        }
-    }
-
-    private List<ArticleDto> getAllArticles() {
-        List<ArticleEntity> articleEntities = articleRepository.findAll();
-        return toArticles(articleEntities);
-    }
-
-    private List<ArticleDto> getFilteredCategoryArticles(String categoryName) {
-        CategoryEntity categoryEntity = this.categoryBusinessService.getCategory(categoryName);
-        List<ArticleEntity> articleEntities = articleRepository.findByCategory(categoryEntity);
-        return toArticles(articleEntities);
-    }
-
-    private List<ArticleDto> toArticles(List<ArticleEntity> articleEntities) {
-        List<ArticleDto> articleDtoList = new ArrayList<>();
-        articleEntities.stream().forEach(ae -> {
-
-            long articleId = ae.getArticleId();
-            String articleName = ae.getArticleName();
-            String categoryName = ae.getCategory().getCategoryName();
-
-            ArticleDto articleDto = new ArticleDto(articleId, articleName, categoryName);
-            articleDtoList.add(articleDto);
-        });
-        return articleDtoList;
+        return articleTransformerService.toArticles(articleEntities);
     }
 }
---
+++ webapp/src/main/java/com/henning/donkey/business/transformer/ArticleTransformerService.java
@@ -0,0 +1,31 @@
+package com.henning.donkey.business.transformer;
+
+import com.henning.donkey.business.responseEnties.ArticleDto;
+import com.henning.donkey.domain.article.ArticleEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ArticleTransformerService {
+
+    public List<ArticleDto> toArticles(List<ArticleEntity> articleEntities) {
+        List<ArticleDto> articleDtoList = new ArrayList<>();
+        articleEntities.stream().forEach(ae -> {
+
+            ArticleDto articleDto = getArticleDto(ae);
+            articleDtoList.add(articleDto);
+        });
+        return articleDtoList;
+    }
+
+    private ArticleDto getArticleDto(ArticleEntity ae) {
+        long articleId = ae.getArticleId();
+        String articleName = ae.getArticleName();
+        String categoryName = ae.getCategory().getCategoryName();
+
+        return new ArticleDto(articleId, articleName, categoryName);
+    }
+
+}
