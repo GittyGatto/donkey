67f3d3cd1dd5a8e6d36fb8d43ce2dbbe0876e042
GittyGatto <rhenning80@gmx.de>
2018-05-18 15:24:31 +0200

    Added CartArticles to cartDto.

--- webapp/src/main/java/com/henning/donkey/business/responseEnties/CartDto.java
+++ webapp/src/main/java/com/henning/donkey/business/responseEnties/CartDto.java
@@ -1,30 +1,42 @@
 package com.henning.donkey.business.responseEnties;
 
+import java.util.List;
+
 public class CartDto
 {
 	private String uuid;
 	private String name;
+	private List<CartArticleDto> cartArticles;
 
 	public CartDto(){}
 
-	public CartDto(String uuid, String name) {
-		this.name = name;
-		this.uuid = uuid;
-	}
+    public CartDto(String uuid, String name, List<CartArticleDto> cartArticles) {
+        this.uuid = uuid;
+        this.name = name;
+        this.cartArticles = cartArticles;
+    }
 
-	public String getUuid() {
-		return uuid;
-	}
+    public String getUuid() {
+        return uuid;
+    }
 
-	public void setUuid(String uuid) {
-		this.uuid = uuid;
-	}
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<CartArticleDto> getCartArticles() {
+        return cartArticles;
+    }
+
+    public void setCartArticles(List<CartArticleDto> cartArticles) {
+        this.cartArticles = cartArticles;
+    }
 }
