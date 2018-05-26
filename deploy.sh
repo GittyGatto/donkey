580a30daf0b82ca0dcc914aef1ec1b86d5b2a4a1
GittyGatto <rhenning80@gmx.de>
2018-05-26 22:06:00 +0200

    deploy script added.

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+message="deployment_`date +%F_%H:%M:%S:%N`"
+
+mvn clean install &&
+cp -v ./webapp/target/donkey-webapp.war ../donkeyDemo/. &&
+cd ../donkeyDemo/ &&
+git add -A &&
+git commit -m $message &&
+git push heroku master &&
+echo "Heroku master pushed, Sir. What a beautiful day!"
