7a3ae39bf51848e2b1c4414878fbf7ce00eb7ac7
GittyGatto <rhenning80@gmx.de>
2018-07-13 17:15:28 +0200

    Reworked articles and categories.

--- webapp/src/main/resources/db/migrations/20180306_2150__add_cart_article.sql
+++ webapp/src/main/resources/db/migrations/20180306_2150__add_cart_article.sql
@@ -5,16 +5,3 @@ create table cart_article (
   article_id bigint,
   amount integer not null
 );
-
-insert into cart_article (cart_id, cart_article_uuid, article_id, amount) VALUES (1, '02376075-5569-4829-88f3-1a63eb6061d9',1,2);
-insert into cart_article (cart_id, cart_article_uuid,article_id, amount) VALUES (1,'b1f0206e-01cf-40a3-8de6-2623ad5370eb', 2,5);
-insert into cart_article (cart_id, cart_article_uuid,article_id, amount) VALUES (1,'6086e26f-e65e-45e7-89a6-08ce2783c4e33', 3,2);
-insert into cart_article (cart_id, cart_article_uuid,article_id, amount) VALUES (1, '9315bfc8-aac7-4bb2-8288-fd163c4458e5', 4,4);
-insert into cart_article (cart_id, cart_article_uuid,article_id, amount) VALUES (1,'b0cd9e68-0a2f-4a3b-87f1-2ecba7502120', 5,1);
-insert into cart_article (cart_id, cart_article_uuid,article_id, amount) VALUES (1,'1f394fb5-c6c7-490e-9d3c-deb793e2bfcb',6,12);
-
-
-
-insert into cart_article (cart_id, cart_article_uuid,article_id, amount) VALUES (2,'4870b056-c0a2-44d0-b2f8-7c9bed702c16',3,3);
-insert into cart_article (cart_id, cart_article_uuid,article_id, amount) VALUES (2,'7fa2d593-3e6a-49fa-bc6a-e22da937af1a',6,1);
-insert into cart_article (cart_id, cart_article_uuid,article_id, amount) VALUES (2,'fb054965-0952-4963-a4b8-0e77bfaceaff',9,5);
---
+++ webapp/src/main/resources/db/migrations/20180612_2028__add_cats_and_artitcles.sql
@@ -0,0 +1,155 @@
+
+--Drinks
+insert into category (category_uuid, category_name) VALUES ('f4d014d0-b1bf-48e3-b2b5-0b75072f5d64', 'Trinken');
+insert into article (article_uuid, article_name, category_id) VALUES ('dee8f939-e825-4757-861a-354bfaae4585', 'Apfelsaft', 1);
+insert into article (article_uuid, article_name, category_id) VALUES ('d3aefa4f-7f9e-4faa-a614-aa4956212bda', 'Espresso', 1);
+insert into article (article_uuid, article_name, category_id) VALUES ('fcac29e7-f16e-4d37-a783-9547065dac7b', 'Kaffee', 1);
+insert into article (article_uuid, article_name, category_id) VALUES ('c29e009f-1514-4c06-bfc4-4bc1de8e3c94', 'Milch', 1);
+insert into article (article_uuid, article_name, category_id) VALUES ('c8a94959-46e6-47e9-8292-612e87dea639', 'Mangosaft', 1);
+insert into article (article_uuid, article_name, category_id) VALUES ('9b95b2b2-34cb-4641-aa40-7a3fd0350cad', 'Tee', 1);
+
+--Meat
+insert into category (category_uuid, category_name) VALUES ('a7714d9d-3069-4999-b515-cbd7feda2791', 'Fleisch');
+insert into article (article_uuid, article_name, category_id) VALUES ('3ec3d276-0af8-4eb8-ab9b-e6fef70725e5', 'Hack', 2);
+insert into article (article_uuid, article_name, category_id) VALUES ('5122b1f5-59e0-4e54-9dcf-9bde2552e1dc', 'Hänchen', 2);
+insert into article (article_uuid, article_name, category_id) VALUES ('bb908f9e-ec89-4bcd-95db-c124c94665b0', 'Leberwurst', 2);
+insert into article (article_uuid, article_name, category_id) VALUES ('e0478e01-c066-45c0-b584-db14b59499c2', 'Prosciutto', 2);
+insert into article (article_uuid, article_name, category_id) VALUES ('ca6fda54-1198-411e-95a9-61ca0207c137', 'Rindergulasch', 2);
+insert into article (article_uuid, article_name, category_id) VALUES ('f67a8422-d8b8-41a3-8445-83f635e2810b', 'Salami', 2);
+insert into article (article_uuid, article_name, category_id) VALUES ('f25d6b8f-4ffc-49bf-b987-74f42a2821ac', 'Schweinemedaillons', 2);
+insert into article (article_uuid, article_name, category_id) VALUES ('849f3f2b-b292-47f6-9829-699f2fe75e50', 'Suppenhuhn', 2);
+insert into article (article_uuid, article_name, category_id) VALUES ('38cfd70b-62d4-4503-996b-811ca4c1fa7e', 'Würstchen', 2);
+
+--Fish
+insert into category (category_uuid, category_name) VALUES ('ea01ea28-144f-4a09-ae3a-36dd05c862f7', 'Fisch');
+insert into article (article_uuid, article_name, category_id) VALUES ('3e09631d-8aee-44a2-837a-c861436a6e90', 'Fischstäbchen', 3);
+
+--Vegetables
+insert into category (category_uuid, category_name) VALUES ('52b5a19d-319a-4300-9c64-1c380349ccec', 'Gemüse');
+insert into article (article_uuid, article_name, category_id) VALUES ('94cb3385-7817-46fc-a8e1-f4bfc19c26ec', 'Aubergine', 4);
+insert into article (article_uuid, article_name, category_id) VALUES ('6152bdd1-e5f8-4a1f-ad9d-d19374e8a76f', 'Blumenkohl', 4);
+insert into article (article_uuid, article_name, category_id) VALUES ('30e07d3f-e872-423a-b867-25270c3b68fa', 'Brokkoli', 4);
+insert into article (article_uuid, article_name, category_id) VALUES ('151c2666-44e3-4de2-88a0-979bea64862d', 'Gewürzgurke', 4);
+insert into article (article_uuid, article_name, category_id) VALUES ('6829b45c-604c-485e-ab4f-7c375af6e47e', 'Gurke', 4);
+insert into article (article_uuid, article_name, category_id) VALUES ('aee606ec-1317-4c8c-957c-44121b8489d5', 'Karotten', 4);
+insert into article (article_uuid, article_name, category_id) VALUES ('b43cf2d1-c82f-47de-89b4-83e3a2044fdc', 'Paprika', 4);
+insert into article (article_uuid, article_name, category_id) VALUES ('05b055db-edc0-4832-8e7f-9c8b66178f31', 'Rote Beete', 4);
+insert into article (article_uuid, article_name, category_id) VALUES ('ae33e1a1-6ef2-44be-870d-32199feb41f5', 'Spinat', 4);
+insert into article (article_uuid, article_name, category_id) VALUES ('5eff72a7-7e0f-42a1-915f-a5f3c7ec9f60', 'Tomaten', 4);
+insert into article (article_uuid, article_name, category_id) VALUES ('50fb2cdd-939c-4d65-94d4-0ca8d004818b', 'Zucchini', 4);
+insert into article (article_uuid, article_name, category_id) VALUES ('4cdba3ae-3887-4e00-bef9-6511c47b3f85', 'Zwiebeln', 4);
+
+--Fruits
+insert into category (category_uuid, category_name) VALUES ('bfe647ef-8a7f-447c-acaa-6c36857b3476', 'Obst');
+insert into article (article_uuid, article_name, category_id) VALUES ('ab524f47-4af1-4124-a68b-22ef561487bd', 'Äpfel', 5);
+insert into article (article_uuid, article_name, category_id) VALUES ('48d2bf0f-6e96-425c-8caa-2cb624ae7c0b', 'Banana', 5);
+insert into article (article_uuid, article_name, category_id) VALUES ('16496004-8c92-4833-b1ab-64e7eac444e9', 'Annanas', 5);
+insert into article (article_uuid, article_name, category_id) VALUES ('ec4970b6-23d3-4841-af67-fdc063129dd2', 'Arbus', 5);
+insert into article (article_uuid, article_name, category_id) VALUES ('49b684af-e2f2-4df9-84c8-2b0747fa1a33', 'Birne', 5);
+insert into article (article_uuid, article_name, category_id) VALUES ('53399074-f2df-4a14-a396-fb8f1a866e77', 'Erbeeren', 5);
+insert into article (article_uuid, article_name, category_id) VALUES ('7462fe74-a499-4217-9105-6a258c8c8548', 'Honigmelone', 5);
+insert into article (article_uuid, article_name, category_id) VALUES ('115416da-3d32-4de9-93d7-644abb44d703', 'Kiwi', 5);
+insert into article (article_uuid, article_name, category_id) VALUES ('44711e54-22c7-493b-a122-e5a7d6e6ff63', 'Mango', 5);
+insert into article (article_uuid, article_name, category_id) VALUES ('5b2a69b1-cf47-4bc8-bbc4-ec733b2c45ae', 'Weintrauben', 5);
+
+--Dairy
+insert into category (category_uuid, category_name) VALUES ('d4312cbc-0d84-4185-ac98-2e1ea9692ec7', 'Milchprodukte');
+insert into article (article_uuid, article_name, category_id) VALUES ('938c3917-2e88-48ec-9774-e9255c9c91e5', 'Cheddar', 6);
+insert into article (article_uuid, article_name, category_id) VALUES ('88035bf3-1553-492c-bf32-9f1e12cc689b', 'Butter', 6);
+insert into article (article_uuid, article_name, category_id) VALUES ('53cdc161-7df0-43c6-8756-2c2f67d6cf6f', 'Buttermilch', 6);
+insert into article (article_uuid, article_name, category_id) VALUES ('8788b0d1-93ee-4e65-bda4-c40a57479da8', 'Joghurt', 6);
+insert into article (article_uuid, article_name, category_id) VALUES ('2507e490-b31c-4e2c-80cf-6b951447dd62', 'Stinkekäse', 6);
+
+--Bread
+insert into category (category_uuid, category_name) VALUES ('e3262af1-9458-4b2b-b923-55d0313bdac9', 'Brot');
+insert into article (article_uuid, article_name, category_id) VALUES ('9c738a1c-5d1c-454f-a91c-556fa93cd089', 'Baguette', 7);
+insert into article (article_uuid, article_name, category_id) VALUES ('dc52a35c-4542-462d-b040-d9982ce45b79', 'Brezel', 7);
+insert into article (article_uuid, article_name, category_id) VALUES ('884671af-7189-4e4d-833f-03ed61d048d7', 'Brot', 7);
+insert into article (article_uuid, article_name, category_id) VALUES ('f1d691a9-a5e7-4a2b-bbe3-f103b4b7d1ad', 'Brötchen', 7);
+insert into article (article_uuid, article_name, category_id) VALUES ('7136d6f1-ea3d-417f-a686-aeae510df183', 'Croissant', 7);
+insert into article (article_uuid, article_name, category_id) VALUES ('87bdc7d7-914f-4074-97b4-aaba97293109', 'Toast', 7);
+
+--Pasta and rice
+insert into category (category_uuid, category_name) VALUES ('4cf26edc-25e1-4a3a-a67a-e5126c946748', 'Pasta und Reis');
+insert into article (article_uuid, article_name, category_id) VALUES ('bc152815-5955-4fd2-8bd1-ca1bfc11d74d', 'Nudeln', 8);
+insert into article (article_uuid, article_name, category_id) VALUES ('527333b9-228c-4ab8-b1e6-3d97a2ca22aa', 'Reis', 8);
+
+--Convenience
+insert into category (category_uuid, category_name) VALUES ('1ddd0c3c-2e82-4289-bb2a-185868451dff', 'Fertigprodukte');
+insert into article (article_uuid, article_name, category_id) VALUES ('fd42d001-99e6-4257-a0d7-ceb27945d4d5', 'Pizza', 9);
+insert into article (article_uuid, article_name, category_id) VALUES ('75491080-b944-44c1-8eb2-fafbd1775985', 'Ravioli', 9);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--Cereal
+insert into category (category_uuid, category_name) VALUES ('07423644-af3c-4246-ace4-666a21074b9a', 'Müsli und Co.');
+insert into article (article_uuid, article_name, category_id) VALUES ('828ea7a8-f20f-4a24-a788-69bfa3992ac4', 'Cornflakes', 10);
+insert into article (article_uuid, article_name, category_id) VALUES ('ea62796e-aeb7-432d-97a9-4014a77da282', 'Müsli', 10);
+
+--Basics
+insert into category (category_uuid, category_name) VALUES ('f9088e26-2926-4a95-a0c3-0320036d4f01', 'Küchen Basics');
+insert into article (article_uuid, article_name, category_id) VALUES ('01252aaf-ea76-4782-86f1-0eed23f234d7', 'Gewürz', 11);
+insert into article (article_uuid, article_name, category_id) VALUES ('b00db367-f12b-48f5-88f0-c738054c77eb', 'Mehl', 11);
+insert into article (article_uuid, article_name, category_id) VALUES ('dc4919a8-ff45-4900-90a3-efbe74a5d420', 'Olivenöl', 11);
+insert into article (article_uuid, article_name, category_id) VALUES ('657cc0a2-f61e-4ca8-bd97-155d6e48dd88', 'Pfeffer', 11);
+insert into article (article_uuid, article_name, category_id) VALUES ('f56372df-6676-4947-8053-033330700c4a', 'Rapsöl', 11);
+insert into article (article_uuid, article_name, category_id) VALUES ('2592a5e4-0e46-405f-a63a-1e23d5b795c5', 'Salz', 11);
+insert into article (article_uuid, article_name, category_id) VALUES ('d4d72c60-f382-4129-805b-a052c1a1a348', 'Tomatenmark', 11);
+insert into article (article_uuid, article_name, category_id) VALUES ('27987389-9f77-49e6-aa93-6d0673b8159f', 'Zucker', 11);
+
+--Sweets
+insert into category (category_uuid, category_name) VALUES ('', 'Süßes');
+insert into article (article_uuid, article_name, category_id) VALUES ('f963b2c1-ae73-48de-a6ee-f926950b3552', 'Chips', 12);
+insert into article (article_uuid, article_name, category_id) VALUES ('ba28288e-f83a-4eca-8c08-b2619116538d', 'Gummibärchen', 12);
+insert into article (article_uuid, article_name, category_id) VALUES ('22e3602d-b131-42d6-bc17-9e6e58a15b53', 'Kekse', 12);
+insert into article (article_uuid, article_name, category_id) VALUES ('1c1aade9-81c3-4d54-8d87-4cf0f2d7329d', 'Nüsse', 12);
+insert into article (article_uuid, article_name, category_id) VALUES ('ef8475d8-170e-41a4-a5d7-b9bdc8fbe880', 'Schokolade', 12);
+insert into article (article_uuid, article_name, category_id) VALUES ('7a8f29d5-e955-41cc-affd-af4453764de1', 'Ü-Ei', 12);
+
+--Baby
+insert into category (category_uuid, category_name) VALUES ('82d0984a-631e-4d94-9fd8-52368df95947', 'Putschka');
+insert into article (article_uuid, article_name, category_id) VALUES ('e6e32719-4d9f-4c5b-bb93-bc7df7474006', 'Baby-Spüli', 13);
+insert into article (article_uuid, article_name, category_id) VALUES ('ae37fb27-4758-497c-aac3-453ac40411fd', 'Glässchen', 13);
+insert into article (article_uuid, article_name, category_id) VALUES ('f5bb892a-6417-4a7a-8be4-33d84192e131', 'Windeln',13);
+
+--Household
+insert into category (category_uuid, category_name) VALUES ('185a3232-b51b-404a-a69c-4bff0f8b6480', 'Haushalt');
+insert into article (article_uuid, article_name, category_id) VALUES ('4fbab9c0-adf7-48fd-8a6b-a60b6a78163d', 'Bodenreiniger', 14);
+insert into article (article_uuid, article_name, category_id) VALUES ('78364145-6399-4bd7-a1d2-8364f97f6ca6', 'Geschir-Taps', 14);
+insert into article (article_uuid, article_name, category_id) VALUES ('bbea4ceb-bd71-418b-8f85-88871e598954', 'Glasreiniger', 14);
+insert into article (article_uuid, article_name, category_id) VALUES ('a2652547-919a-473b-b023-6dcf56db7dea', 'Waschmittel', 14);
+
+--Body care
+insert into category (category_uuid, category_name) VALUES ('d8e681dd-2cf4-4bec-8543-dedbe3110ba0', 'Körperpflege');
+insert into article (article_uuid, article_name, category_id) VALUES ('591567b4-dd02-4fd7-849d-16215dc36fce', 'Colgate', 15);
+insert into article (article_uuid, article_name, category_id) VALUES ('769bddcd-daa8-4d5c-9626-1a79f12f29f5', 'Duschgel', 15);
+insert into article (article_uuid, article_name, category_id) VALUES ('101c4eee-8bb3-4e1a-a0c7-42f7573fbfd3', 'Elmex', 15);
+insert into article (article_uuid, article_name, category_id) VALUES ('f148f5ef-5df9-47c6-b019-55c9846f90c4', 'Head and Shoulders', 15);
+insert into article (article_uuid, article_name, category_id) VALUES ('f3f2e6a4-e170-4b29-95f3-62becd4070c8', 'Niva Dry Impact', 15);
+
+--Alc
+insert into category (category_uuid, category_name) VALUES ('bd6be678-05cb-4b50-807a-6d27886c42cd', 'Alkohol');
+insert into article (article_uuid, article_name, category_id) VALUES ('59120433-dc56-4580-9ce4-2b265ec95228', 'Bier', 16);
+insert into article (article_uuid, article_name, category_id) VALUES ('33bcca2e-9df9-49d7-a4d4-87ffa70f1791', 'Radler', 16);
+insert into article (article_uuid, article_name, category_id) VALUES ('9adbd197-a983-4423-91c2-61d1f5b4bd99', 'Sekt', 16);
+insert into article (article_uuid, article_name, category_id) VALUES ('87e7f210-c372-42c7-9b8f-04566d3125a0', 'Wein', 16);
+insert into article (article_uuid, article_name, category_id) VALUES ('a8a17c67-2b3f-45b9-a7d8-7600ea2b32f9', 'Wodka', 16);
+
+--Smoke
+insert into category (category_uuid, category_name) VALUES ('ed0f67f4-14a6-4c54-b2a4-15a42f4cd57a', 'Rauchen');
+insert into article (article_uuid, article_name, category_id) VALUES ('b50300b3-4acc-4c5b-9fca-754b1ab40dcb', 'Filters', 17);
+insert into article (article_uuid, article_name, category_id) VALUES ('0da5d892-26b5-46f5-9f1a-3b96c5ead826', 'Pueblo Tabak', 17);
+insert into article (article_uuid, article_name, category_id) VALUES ('8805755f-f6dc-4bd5-9e8a-ec248c68fed8', 'Papers', 17);
